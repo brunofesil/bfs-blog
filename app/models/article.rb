2e7f73c86fd5db4761e3ddd17749e7437aba7616
brunofesil <brfsilva@outlook.com>
2020-07-07 23:20:20 -0300

    Style show view, almost like index view.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ApplicationRecord
-  validates :title, presence: true, length: {minimum:10, maximum:150}
+  validates :title, presence: true, length: {minimum: 6, maximum:150}
   validates :description, presence: true, length: {minimum:10, maximum:2000}
 end

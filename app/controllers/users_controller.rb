a2a00986ff4abcc8c379ea87431ab8c19d77d286
brunofesil <brfsilva@outlook.com>
2020-08-26 21:59:44 -0300

    Clean up layout and Added pagination using will-paginate gem

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,14 @@
 class UsersController < ApplicationController 
 
+  def index
+    @user = User.paginate(page: params[:page], per_page: 5)
+  end
+
   def show
     @user = User.find(params[:id])
-    @article = @user.articles
+    @article = @user.articles.paginate(page: params[:page], per_page: 5)
   end
   
-
   def new
     @user = User.new
   end
@@ -18,7 +21,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "Your account information was successfully updated."
-      redirect_to articles_path
+      redirect_to user_path
     else
       render 'edit'
     end

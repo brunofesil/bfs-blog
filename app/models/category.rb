1b8a8ad1a48a8b874f5debb75bd99e59d59f19fd
brunofesil <brfsilva@outlook.com>
2020-09-27 15:46:27 -0300

    Category's new template

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,4 @@
 class Category < ApplicationRecord
-  validates :name, presence: true, length: {minimum:3, maximum:25}
-  validates_uniqueness_of :name, on: :create, message: "must be unique"
+  validates :name, presence: true, length: {minimum:3, maximum:25}, uniqueness: { case_sensitive: false } 
   
 end

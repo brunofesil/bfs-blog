12d90f9cc8da2405678a4c44315d602cdaa885df
brunofesil <brfsilva@outlook.com>
2020-09-28 23:09:52 -0300

    Integration test for invalid category

---
+++ test/integration/list_category_test.rb
@@ -0,0 +1,16 @@
+require 'test_helper'
+
+class ListCategoryTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @category = Category.create(name: "Science")
+    @category2 = Category.create(name: "Recreation")
+  end
+  
+  test "should show categories listing" do
+    get '/categories'
+    assert_select "a[href=?]", category_path(@category), text: @category.name
+    assert_select "a[href=?]", category_path(@category2), text: @category2.name
+  end
+
+end
